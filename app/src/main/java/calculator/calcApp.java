5bb42a7a32a3471e920ebbc9112d4656e3cbde41
Bryan Alvarado-Perez <143572429+Bryan-Alvarado-Perez@users.noreply.github.com>
2024-09-22 23:23:09 -0700

    fixed syntax errors, imported scanner,  adding add function

--- app/src/main/java/calculator/calcApp.java
+++ app/src/main/java/calculator/calcApp.java
@@ -1,30 +1,35 @@
 package calculator;
+import java.util.Scanner;
 
 public class calcApp {
     
     public class Calculator {
 
-        private num1 = 0;
-        private num2 = 0;
-        private result = 0;
+        private StringBuilder userInput = new StringBuilder();
+        private float num1;
+        private float num2;
+        private float result;
 
-        public Calculator() {
-            this.num1 = num1;
-            this.num2 = num2;
-            this.result = result;
+        public Calculator(float num1, float num2, float result) {
+            this.num1 = 0;
+            this.num2 = 0;
+            this.result = 0;
         }
 
 
-
-
+        public float add() {
+            return this.result = this.num1 + this.num2;
+        }
     }
 
-    public static void main(String[] args, input) {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String userInput = "";
+
         while (userInput != "q") {
-            Scanner input = new Scanner(System.in);
-            userInput = "";
             Menu.displayMenu();
             userInput = Menu.getUserInput(input);
         }
+        input.close();
     }
 }
