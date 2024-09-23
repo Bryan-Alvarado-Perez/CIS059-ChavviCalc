40cb1bac4fbe33025ade2898bee730d812423e28
Bryan Alvarado-Perez <143572429+Bryan-Alvarado-Perez@users.noreply.github.com>
2024-09-22 23:12:14 -0700

    Created variables and main

---
+++ app/src/main/java/calculator/calcApp.java
@@ -0,0 +1,30 @@
+package calculator;
+
+public class calcApp {
+    
+    public class Calculator {
+
+        private num1 = 0;
+        private num2 = 0;
+        private result = 0;
+
+        public Calculator() {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.result = result;
+        }
+
+
+
+
+    }
+
+    public static void main(String[] args, input) {
+        while (userInput != "q") {
+            Scanner input = new Scanner(System.in);
+            userInput = "";
+            Menu.displayMenu();
+            userInput = Menu.getUserInput(input);
+        }
+    }
+}
