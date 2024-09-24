53ba05278a0176dad8cce48b14caef8b2efdafdf
Bryan Alvarado-Perez <143572429+Bryan-Alvarado-Perez@users.noreply.github.com>
2024-09-23 20:30:58 -0700

    completed methods for Menu and Calculator class
    Calculator Class:
    add()
    subtract()
    divide()
    clearValues()
    setNum1()
    setNum2()
    
    Menu Class:
    finished the switch cases
    added getUserInput()

--- app/src/main/java/calculator/calcApp.java
+++ app/src/main/java/calculator/calcApp.java
@@ -3,26 +3,96 @@ import java.util.Scanner;
 
 public class calcApp {
     
-    public class Calculator {
-
+    public static class Calculator {
+        
         private float num1;
         private float num2;
-        private float result;
 
-        public Calculator(float num1, float num2) {
+        public void setNum1(float num1) {
             this.num1 = num1;
-            this.num2 = num2;
-            this.result = 0;
         }
 
-        public float add() {
-            result = num1 + num2;
-            return result;
+        public void setNum2(float num2) {
+            this.num2 = num2;
+        }
+
+        public void add() {
+            System.out.println("Addition result: " + (num1 + num2));
+        }
+
+        public void subtract() {
+            System.out.println("Subtraction result: " + (num1 - num2));
+        }
+
+        public void multiply() {
+            System.out.println("Multiplication result: " + (num1 * num2));
+        }
+
+        public void divide() {
+            if (num2 == 0) {
+                System.out.println("You can't divide by zero");
+            } else {
+                System.out.println("Division result: " + (num1 / num2));
+            }
+        }
+
+        public void clearValues() {
+            num1 = 0;
+            num2 = 0;
+            System.out.println("The values have been cleared");
         }
     }
-    
-        public class Menu {
+
+        public static class Menu {
+            private static Calculator calculator = new Calculator();
+            public static void handleCases(String userInput, Scanner input) {
+                 switch(userInput) {
+                    case "1":
+                        System.out.println("Enter the first number: ");
+                        calculator.setNum1(input.nextFloat());
+                        input.nextLine();
+                        break;
+
+                    case "2":
+                        System.out.println("Enter the second number: ");
+                        calculator.setNum2(input.nextFloat());
+                        input.nextLine();
+                        break;
+
+                    case "a":
+                        calculator.add();
+                        break;
+                    
+                    case "s":
+                        calculator.subtract();
+                        break;
+                    
+                    case "m":
+                        calculator.multiply();
+                        break;
+
+                    case "d":
+                        calculator.divide();
+                        break;
+
+                    case "c":
+                        calculator.clearValues();
+                        break;
+
+                    case "q":
+                        System.out.println("You have quit the calculator");
+                        break;
+
+                    default:
+                        System.out.println("That is an invalid option.");
+                        break;
+                }
+            }
             
+            public static String getUserInput(Scanner input) {
+                return input.nextLine();
+            }
+
             public static void displayMenu() {
                 System.out.println("===============================");
                 System.out.println("|======CHAVVI-CALCULATOR======|");
@@ -37,19 +107,16 @@ public class calcApp {
                 System.out.println("|===[C]-CLEAR-VALUES==========|");
                 System.out.println("===============================");
             }
-
-            public static String getUserInput(Scanner input) {
-                return input.nextLine();
-            }
         }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String userInput = "";
-
         while (!userInput.equals("q")) {
             Menu.displayMenu();
             userInput = Menu.getUserInput(input);
+            userInput = userInput.toLowerCase();
+            Menu.handleCases(userInput, input);
         }
         input.close();
     }
